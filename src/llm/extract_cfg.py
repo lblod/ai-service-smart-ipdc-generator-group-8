b120c81779d2203d621430abdb58f1e460c93acb
Sam Legrand <sam.legrand@soprasteria.com>
2024-09-30 22:20:26 +0200

    Merge procedure prompts with costs and conditions prompts

--- src/llm/extract_cfg.py
+++ src/llm/extract_cfg.py
@@ -1,10 +1,12 @@
 FORMAT_INSTRUCTIONS_KEY = 'format_instructions'
 TEXT_KEY = 'text'
 EXTRACTION_PROMPT = f'''You are an expert in extracting structured data from unstructured text.
-Specifically, you have expert knowledge of so-called 'besluiten', which are decision made by a government (can be on different levels: municipality, province, etc.)
-These documents are in Dutch, but since you are fluent in Dutch, this is no issue. You will extract data from the 'besluiten' in Dutch for a products and services catalog.
+Specifically, you have expert knowledge of so-called 'besluiten', which are decision made by a government (can be on different levels: municipality, province, etc.).
+These documents are in Dutch, but since you are fluent in Dutch, this is no issue. You will extract data from the provided 'besluit' in Dutch for a products and services catalog that is visible to citizens.
 
 Besluit (unstructured text):
 {{{TEXT_KEY}}}
 
-{{{FORMAT_INSTRUCTIONS_KEY}}}'''
+{{{FORMAT_INSTRUCTIONS_KEY}}}
+
+Remember to only provide data extracted from the provided Besluit!'''
--- src/models.py
+++ src/models.py
@@ -1,12 +1,14 @@
 from pydantic import BaseModel, Field
 from typing import List, Optional
 
-IPDC_DESCRIPTION = '''This property represents a free text Description of the Public Service.
+RETURN_INSTRUCTION = 'The result should be returned in Dutch and can only be information or text from the main Besluit.'
+
+IPDC_DESCRIPTION = f'''This property represents a free text Description of the Public Service.
 The description is the text that potential users of the Public Service see in any public service catalogue.
 Public administrations are encouraged to include a reasonable level of detail in the description, for instance including basic eligibility requirements for the particular Public Service and contact information.
-The result should be returned in Dutch and can only be information or text from the main Besluit.'''
+{RETURN_INSTRUCTION}'''
 
-IPDC_NAME = 'This property represents the official Name of the Public Service. The result should be returned in Dutch and can only be information or text from the main Besluit.'
+IPDC_NAME = f'This property represents the official Name of the Public Service. {RETURN_INSTRUCTION}'
 
 IPDC_COST = 'The Cost represents any costs related to the execution of a Public Service that the Agent consuming it needs to pay. The result should be returned in Dutch and can only be information or text from the main Besluit.'
 
@@ -14,6 +16,9 @@ IDPC_CONDITION = '''A normal requirement can specify the expected value that the
 The normal requirement may apply to a certain period of time. It also can provide a list of candidate evidences that the responder can use to prove the normal requirement.
 The result should be returned in Dutch and can only be information or text from the main Besluit.'''
 
+PROCEDURE_DESCRIPTION = f'A text field that outlines the procedure in detail in citizen readable language (do not refer to articles). {RETURN_INSTRUCTION}'
+
+PROCEDURE_NAME = f'The name of the procedure. {RETURN_INSTRUCTION}'
 
 class ProcessingRequest(BaseModel):
     decision_text: str
@@ -25,7 +30,9 @@ class IPDCBaseType(BaseModel):
 
 
 class IPDCProcedure(IPDCBaseType):
-    pass
+    '''Contains the specific rules, guidelines or procedures that need to be followed. It can include the terms of service, licence, and authentication requirements of the Public Service.'''
+    description: str = Field(description=PROCEDURE_DESCRIPTION)
+    name: Optional[str] = Field(description=PROCEDURE_NAME)
 
 
 class IPDCCost(IPDCBaseType):
@@ -43,9 +50,10 @@ class IPDCCondition(IPDCBaseType):
 
 # the LLM will see the descriptions of this class
 class IPDCData(BaseModel):
-    '''Data to include within an IPDC (products and services catalog) entry'''
+    '''Data within an IPDC (products and services catalog) entry. All values within the fields are written in Dutch and contain information coming from the provided Besluit.'''
     description: str = Field(description=IPDC_DESCRIPTION)
     name: Optional[str] = Field(description=IPDC_NAME)
+    procedure: Optional[IPDCProcedure]
     costs: Optional[List[IPDCCost]] = Field(description=IPDC_COST)
     conditions: List[IPDCCondition] = Field(description=IDPC_CONDITION)
 

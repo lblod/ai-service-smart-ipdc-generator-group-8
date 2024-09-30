255e9d12fcbe1abdafa61ab89523e594af903818
Sam Legrand <sam.legrand@soprasteria.com>
2024-09-30 23:48:18 +0200

    change get to post

--- web.py
+++ web.py
@@ -6,6 +6,6 @@ class Result(BaseModel):
     description: str
 
 
-@app.get("/generate-ipdc")
+@app.post("/generate-ipdc")
 def generated_ipdc(request: ProcessingRequest):
     return Result(description='Maatregelen tijdens de crisisperiode voor handelaars: gratis aanvraag voor inname en plaatsing van verkeersborden door gemeentelijke diensten.')
